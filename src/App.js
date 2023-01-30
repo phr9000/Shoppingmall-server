3d9c2814c06aae16b14b2d7e2d44ff843f4d655a
hailan <phr9000@gmail.com>
2023-01-30 20:28:23 +0900

    초기세팅

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import { useState } from "react"
+import Header from "./components/Header/Header"
+
+function App() {
+    return (
+        <>
+            <Header/>
+            {/* <div className="container">
+                <MainLayout/>
+            </div> */}
+        </>     
+        )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "assets/styles/common.scss"
+function Header(){
+    return (
+        <header className="test">
+            <div>test</div>
+        </header>
+    )
+}
+
+export default Header
