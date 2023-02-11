91b9cf00109b82516b6000c9f7725624c5249511
minzoo <zooklimk90@gmail.com>
2023-02-11 20:34:53 +0900

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import '../assets/styles/index.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/index.scss";
 
 export default function Header() {
   return (
-    <header className='header-container'>
-      <span className='display-inblock'><Link to='/guide'>Guide</Link></span>
-      <span className='display-inblock ml5'><Link to='/join'>Join</Link></span>
+    <header className="header-container">
+      <span className="display-inblock">
+        <Link to="/guide">Guide</Link>
+      </span>
+      <span className="display-inblock ml5">
+        <Link to="/join">Join</Link>
+      </span>
+      <div>후덜덜해 이렇게 하는 데 변경이 안되나? 이번에는 성공한다</div>
     </header>
-  )
+  );
 }
