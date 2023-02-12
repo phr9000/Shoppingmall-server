6400e97bde54ec44a0a20950953907ec2a738576
minzoo <zooklimk90@gmail.com>
2023-02-12 11:10:41 +0900

    [로그인]테스트

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
-  return (
-    <div className='login-container'>
-      Login11
-    </div>
-  )
+  return <div className="login-container">Login11 될까 될까 될까</div>;
 }
-
