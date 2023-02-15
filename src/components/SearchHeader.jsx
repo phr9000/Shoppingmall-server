9c79959ea78a4b3eb37b356a7bc3d2397504ca29
hailan <phr9000@gmail.com>
2023-02-15 13:07:16 +0900

    [테스트] 빌드시 이미지 경로 테스트

---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SearchHeader() {
+  return (
+    <div className='search-header-container'>
+        <h1>
+            <a href='/'>
+                <span className='img-test'></span>
+            </a>
+        </h1>
+    </div>
+  )
+}
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom';
 import Header from 'components/Header';
+import SearchHeader from 'components/SearchHeader';
 import '../assets/styles/index.scss';
 
 export default function Root() {
   return (
     <div>
+      <SearchHeader/>
       <Header/>
       <div className='main-layout-container'>
         <Outlet/>
