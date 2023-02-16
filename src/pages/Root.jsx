abe860195e217fcac6c50e005cb93606671dd350
minzoo <zooklimk90@gmail.com>
2023-02-16 23:12:58 +0900

    [푸터] Root 연결

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom';
-import Header from 'components/Header';
-import SearchHeader from 'components/SearchHeader';
-import '../assets/styles/index.scss';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "components/Header";
+import SearchHeader from "components/SearchHeader";
+import "../assets/styles/index.scss";
 
 export default function Root() {
   return (
     <div>
-      <SearchHeader/>
-      <Header/>
-      <div className='main-layout-container'>
-        <Outlet/>
+      <SearchHeader />
+      <Header />
+      <div className="main-layout-container">
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
