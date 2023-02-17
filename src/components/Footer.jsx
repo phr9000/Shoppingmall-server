e0b0513957e849e2c9c5398a8aac1b2266aca5a5
hailan <phr9000@gmail.com>
2023-02-17 11:37:09 +0900

    [푸터] 스타일시트 index.scss로 이동

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from "../assets/styles/layout/footer.module.scss"; // eslint-disable-line no-unused-vars
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <div className={styles.footer__nav}>
+    <div className="footer">
+      <div className="footer__nav">
         <ul>
           <li>회사소개</li>
           <li>입점상담</li>
@@ -16,8 +15,8 @@ export default function Footer() {
           <li>GLOBAL</li>
         </ul>
       </div>
-      <div className={styles.footer__info}>
-        <div className={styles.footer__info__company}>
+      <div className="footer__info">
+        <div className="footer__info__company">
           상호명 : ㈜더블유컨셉코리아 ㅣ 대표자 : 이은철 ㅣ 주소 : 서울특별시
           강남구 테헤란로 231, EAST동 20층(역삼동, 센터필드)
           <br /> 사업자등록번호 : 211-88-19183 ㅣ 통신판매업신고 :
@@ -25,12 +24,12 @@ export default function Footer() {
           개인정보보호책임자 : 허선희 │ 호스팅서비스 : ㈜ 더블유컨셉코리아
           <br /> COPYRIGHT ⓒ ㈜더블유컨셉코리아 ALL RIGHTS RESERVED
         </div>
-        <div className={styles.footer__info__customer}>
+        <div className="footer__info__customer">
           <b>소비자피해보상보험</b>
           <br />
           고객님은 안전거래를 위해 현금 결제 시, Wconcept 에서 가입한
           소비자피해보상보험 서비스를 이용하실 수 있습니다. <br />
-          보상대상 : 미배송/반품, 환불거부/쇼핑몰부도{" "}
+          보상대상 : 미배송/반품, 환불거부/쇼핑몰부도
           <button>서비스 가입사실 확인</button>
         </div>
       </div>
