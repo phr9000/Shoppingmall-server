22ea99daf9471c81f252c5341702a6c894946baf
hailan <phr9000@gmail.com>
2023-03-05 11:56:57 +0900

    [product] Product 컴포넌트 진행중

--- src/pages/mainLayout/MainLayout.jsx
+++ src/pages/mainLayout/MainLayout.jsx
@@ -1,32 +1,35 @@
-import React, {useEffect, useState} from 'react'
+// import React, {useEffect, useState} from 'react'
+import React from 'react'
 import MainCarousel from './MainCarousel';
+import ProductIntro from 'components/ProductIntro';
 
 export default function MainLayout() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   
   // USEEFFECT에 담고, []를 작성해야 한번만 호출됨, 여기 안에 안담기면 무한 루프에 빠짐 
-  useEffect(()=>{
-    fetch('data/products.json')
-    .then(res=>res.json())
-    .then(data=> {
-      console.log('ok');
-      setProducts(data);
-    });
-    return () => {
-      console.log('unmount 될 때 청소하는 일을 담당');
-    };
-  }, []);
+  // useEffect(()=>{
+  //   fetch('data/products.json')
+  //   .then(res=>res.json())
+  //   .then(data=> {
+  //     console.log('ok');
+  //     setProducts(data);
+  //   });
+  //   return () => {
+  //     console.log('unmount 될 때 청소하는 일을 담당');
+  //   };
+  // }, []);
 
   return (
     <div className='main-layout-container'>
       <MainCarousel/>
-      <ul>
+      <ProductIntro/>
+      {/* <ul>
         {
           products.map((product) => 
             <li key={product.id}>{product.name}</li>
           )
         }
-      </ul>
+      </ul> */}
     </div>
   )
 }
