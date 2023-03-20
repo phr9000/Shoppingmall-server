6a69c0b56d78b0af30aaeca0a4164d6f8a41f774
henry <henry@yescnc.co.kr>
2023-03-20 20:57:40 +0900

    new page 중간 푸쉬

--- src/pages/New/NewLayout.tsx
+++ src/pages/New/NewLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { Pagination } from 'antd';
 const NewLayout:React.FC = ()=>{
   return (
     <div>
       NEW
+      <Pagination defaultCurrent={1} total={50} />
     </div>
+
   )
 }
 
