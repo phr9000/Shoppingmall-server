68b4439bc3dd80cd38bcf69d900008af412f237b
DESKTOP-SECUI\yescnc <phr9000@yescnc.co.kr>
2023-04-13 16:46:26 +0900

    preshow 중간푸시

--- src/pages/PreShow/PreshowLayout.tsx
+++ src/pages/PreShow/PreshowLayout.tsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import preShowtData from "assets/data/preshowData.json";
 const PreShowLayout:React.FC = ()=>{
+    const [data, setData] = useState(preShowtData);
+    const hasModern = () => {
+        const resultModern = preShowtData.filter((list) =>
+          list.category.includes("modern")
+        );
+        setData(resultModern);
+      };
     return (
         <div className='main-layout-container preshow-container'>
             <div className='title-area'>
@@ -7,6 +15,20 @@ const PreShowLayout:React.FC = ()=>{
                 <div className="desc">
                     W컨셉의 주요 브랜드의 신상과 트렌드를 먼저 만나볼 수 있어요
                 </div>
+            </div>
+            <div className='preshow-tab'>
+                <ul>
+                    <li>
+                        <button {() => setData(preShowtData)}></button>
+                    </li>
+                    <li>
+                        <button onClick={hasModern}></button>
+                    </li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         </div>
     )
