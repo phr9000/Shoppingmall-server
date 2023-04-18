605e99eb04f2e991a69242c23fce6c86159e84da
hailan <phr9000@gmail.com>
2023-04-19 01:03:09 +0900

    origianl 완료

--- src/pages/Original/OriginalLayout.tsx
+++ src/pages/Original/OriginalLayout.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import originalData from "../../assets/data/originalData.json"
 
 const OriginalLayout:React.FC = ()=> {
     type tabs = {
@@ -42,50 +41,82 @@ const OriginalLayout:React.FC = ()=> {
         {
             "category": "brand",
             "id": 1,
-            "img": "/images/preshow_modern01.jpg",
-            "tag": ["스프링", "데님"],
-            "title": "LE17SEPTEMBPE", 
-            "desc" : "시그니처 미학에 충실한 23봄 두번째 컬렉션"
+            "img": "/images/original_brand01.jpg",
+            "tag": ["아디다스", "ADIDAS"],
+            "title": "BRAND WE LIKE", 
+            "desc" : "아디다스의 부활"
+        },
+        {
+            "category": "brand",
+            "id": 2,
+            "img": "/images/original_brand02.jpg",
+            "tag": ["보카바카", "VOCAVACA","익스클루시브"],
+            "title": "BRAND WE LIKE", 
+            "desc" : "마이 리틀 로맨스"
         },
         {
             "category": "trend",
-            "id": 2,
-            "img": "/images/preshow_modern01.jpg",
+            "id": 3,
+            "img": "/images/original_trend01.jpg",
             "tag": ["스프링", "데님"],
-            "title": "LE17SEPTEMBPE", 
-            "desc" : "시그니처 미학에 충실한 23봄 두번째 컬렉션"
+            "title": "TREND WE PICK", 
+            "desc" : "올봄, 당신에게 꼭 필요한 데님"
+        },
+        {
+            "category": "trend",
+            "id": 4,
+            "img": "/images/original_trend02.jpg",
+            "tag": ["스프링", "men","맨통합"],
+            "title": "TREND WE PICK", 
+            "desc" : "더 자유로울 수 있게"
         },
         {
             "category": "women",
-            "id": 3,
-            "img": "/images/preshow_modern01.jpg",
-            "tag": ["스프링", "데님"],
-            "title": "LE17SEPTEMBPE", 
-            "desc" : "시그니처 미학에 충실한 23봄 두번째 컬렉션"
+            "id": 5,
+            "img": "/images/original_women01.jpg",
+            "tag": ["닐비아피", "정은채","NILBYP"],
+            "title": "WOMEN WE LOVE", 
+            "desc" : "정은채와 함께한 어느 봄날"
+        },
+        {
+            "category": "women",
+            "id": 6,
+            "img": "/images/original_women02.jpg",
+            "tag": ["던스트", "Dunst","23SS"],
+            "title": "WOMEN WE LOVE", 
+            "desc" : "던스트가 사랑한 지수"
         },
         {
             "category": "love",
-            "id": 4,
-            "img": "/images/preshow_modern01.jpg",
-            "tag": ["스프링", "데님"],
-            "title": "LE17SEPTEMBPE", 
-            "desc" : "시그니처 미학에 충실한 23봄 두번째 컬렉션"
+            "id": 7,
+            "img": "/images/original_love01.jpg",
+            "tag": ["화이트데이", "LOVE"],
+            "title": "LOVE WE MEET", 
+            "desc" : "다샤, 조나단 부부의 특별한 일상"
+        },
+        {
+            "category": "love",
+            "id": 8,
+            "img": "/images/original_love02.jpg",
+            "tag": ["화이트데이", "LOVE"],
+            "title": "LOVE WE MEET", 
+            "desc" : "헤이주패밀리 화이트데이 홈파티"
         },
         {
             "category": "place",
-            "id": 2,
-            "img": "/images/preshow_modern01.jpg",
-            "tag": ["스프링", "데님"],
-            "title": "LE17SEPTEMBPE", 
-            "desc" : "시그니처 미학에 충실한 23봄 두번째 컬렉션"
+            "id": 9,
+            "img": "/images/original_place01.jpg",
+            "tag": ["을지로맛집", "W핫플","더블유핫플","을지로전축","전축","LP바"],
+            "title": "PLACE WE CHILL", 
+            "desc" : "Whotple 전축"
         },
         {
-            "category": "trend",
-            "id": 5,
-            "img": "/images/preshow_modern01.jpg",
-            "tag": ["스프링", "데님"],
-            "title": "LE17SEPTEMBPE", 
-            "desc" : "시그니처 미학에 충실한 23봄 두번째 컬렉션"
+            "category": "place",
+            "id": 10,
+            "img": "/images/original_place02.jpg",
+            "tag": ["에치세로소스", "회현맛집","Whotple","더블유핫플"],
+            "title": "PLACE WE CHILL", 
+            "desc" : "Whotple 에치세로 소스"
         }
     ]);
     type Product = {
@@ -128,7 +159,36 @@ const OriginalLayout:React.FC = ()=> {
             <div className='content-area'>
                 <ul>
                     {(filteredProducts ?? products).map((product) => (
-                        <li key={product.id}>{product.title}</li>
+                        <li key={product.id}>
+                            <a href="!#">
+                                <div className="img-area">
+                                    <img src={product.img} alt="" />
+                                </div>
+                                <div className="txt-area">
+                                    <h3>
+                                        {product.title}
+                                    </h3>
+                                    <p>
+                                        {product.desc}
+                                    </p>
+                                    <div className='tag-area flex'>
+                                        <h4>#TAG</h4>
+                                        <ul className='tag'>
+                                            
+                                            {product.tag.map((tags) =>(
+                                                <li key={tags}>
+                                                    <button>
+                                                        {tags}
+                                                    </button>
+                                                </li>
+                                                )
+                                            )}
+                                               
+                                        </ul>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
                     ))}
                 </ul>
             </div>
